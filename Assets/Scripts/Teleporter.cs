6f0ff72e26fdfddebf0d21f0f7784203f08bf266
Mihail Morosan <mmoro001@cgs-macd-15505.campus.goldsmiths.ac.uk>
2014-11-24 11:57:35 +0000

    Teleporters are now prefabs (start and end location). Teleporter code now takes this in consideration.

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class Teleporter : MonoBehaviour {
 
-	public GameObject player;
+	//public GameObject player;
 
 	public GameObject teleportPoint;
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Player")
+		//if (teleportPoint != null && other.gameObject.tag == "Player")
+        if (teleportPoint != null)
 		{
-			other.gameObject.transform.position = new Vector3(2, 10 , -23);
-
+			//other.gameObject.transform.position = new Vector3(2, 10 , -23);
+            other.gameObject.transform.position = teleportPoint.transform.position;
 		}
 }
 }
