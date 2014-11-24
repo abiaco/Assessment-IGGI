708a1b78379677da53884c044910a821f44d1c51
Kokkinakis <avk504@york.ac.uk>
2014-11-24 14:13:37 +0200

    simple deletion script that can be atttached to objects

---
+++ Assets/Scripts/Destroy_Object.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_Object : MonoBehaviour {
+
+	public float Simple_TIME_LEFT;
+
+
+	void OnCollisionEnter(Collision Liz){
+		Debug.Log ("ENTRY COLLISION");
+		if (Liz.gameObject.name == "Player") {
+			Destroy(gameObject, Simple_TIME_LEFT);
+		}
+	}
+}
