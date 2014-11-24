8817ed4a494a9fb703f914e5f859ee3b69093c9d
Kokkinakis <avk504@york.ac.uk>
2014-11-24 13:43:29 +0200

    Added jumping to player

--- Assets/Scripts/Player_Control.cs
+++ Assets/Scripts/Player_Control.cs
@@ -8,10 +8,16 @@ public class Player_Control : MonoBehaviour {
 	public float speed;
 	public GUIText countText;
 	private int count;
+	private float Jump;
+	private float Double_Jump;
+	private bool first_jump_pressed;
+	public float Jump_Height;
 
 	void Start() {
 				count = 0;
 				setCountText();
+		first_jump_pressed = false;
+
 		}
 	 //Update is called once per frame
 
@@ -22,7 +28,43 @@ public class Player_Control : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
+//		Jump = Input.GetKey ("space");
+
+		if (Input.GetKeyDown ("space") && first_jump_pressed == false) {
+						Debug.Log ("SPACE HAS BEEN PRESSED");
+						rigidbody.AddForce(Vector3.up * Jump_Height);
+			
+		}
+
+
+
+			//if (first_jump_pressed == true) {
+
+
+
+		      //Replace_Vector3.up and it makes a dash !
+		//}
+
 	}
+
+
+	void OnCollisionExit(Collision col) {
+
+				Debug.Log ("COLLISION");
+		if ((col.gameObject.name == "Ground") && first_jump_pressed == false) {
+						first_jump_pressed = true;
+//						if (first_jump_pressed = true) {
+//								first_jump_pressed = false;
+//						}
+				}
+		}
+	void OnCollisionEnter(Collision lol){
+		Debug.Log ("ENTRY COLLISION");
+			if ((lol.gameObject.name == "Ground") && first_jump_pressed ==true) {
+				first_jump_pressed=false;
+			}
+		}
+
 	void OnTriggerEnter(Collider other) {
 				if (other.gameObject.tag == "Pickup") {
 						other.gameObject.SetActive (false);
