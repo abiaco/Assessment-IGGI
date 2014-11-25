cfbc1909460d9e86a75418d4fa3e99e89e914c32
Mihail Morosan <mmoro001@cgs-macd-15506.campus.goldsmiths.ac.uk>
2014-11-25 13:13:11 +0000

    Added initial Victory Condition script. Has ArriveAtDestination working. Updated player prefab accordingly.

---
+++ Assets/Scripts/VictoryConditions.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum VictoryType
+{
+    ArriveAtDestination = 1,
+    CollectAllItems = 2,
+    TouchLocationsInOrder = 3
+}
+
+public class VictoryConditions : MonoBehaviour {
+
+    public VictoryType VictoryType = VictoryType.ArriveAtDestination;
+    
+    public GameObject Destination;
+    
+    public GameObject[] ItemsToCollect;
+    
+    public GameObject[] LocationsToTouch;
+    
+    public int TriggerRange = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(VictoryType == VictoryType.ArriveAtDestination)
+        {
+            if(Vector3.Distance(transform.position, Destination.transform.position) <= TriggerRange)
+            {
+                //Victory
+                Debug.Log("You win.");
+            }
+        }
+	}
+}
