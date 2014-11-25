54ffd6665a07e43133e2a3a17d405660a5fa0309
Kokkinakis <avk504@york.ac.uk>
2014-11-25 16:09:39 +0200

    Time delay when pressing q can be used to make pause button

---
+++ Assets/Scripts/Time_Delay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//private bool Right_click_Pressed;
+
+//although in theory a pause button can be made by reducing Time.timeScale to 0
+// it is not that simple since certain unfinished player actions will still happen 
+
+public class Time_Delay : MonoBehaviour {
+	
+	void Update() {
+		if (Input.GetKeyDown("q")) {   //&& Right_click_Pressed == false)
+			if (Time.timeScale == 1.0F) //realtime = 1
+				Time.timeScale = 0.3F;  // the lower it is the slower time moves
+				else
+				Time.timeScale = 1.0F;
+				Time.fixedDeltaTime = 0.02F * Time.timeScale;
+			}
+	}
+}
