686ff0938d5197b319941102965b47477563e9ba
Mihail Morosan <mmoro001@cgs-macd-15506.campus.goldsmiths.ac.uk>
2014-11-25 13:28:17 +0000

    Added CollectAllItems win condition. Updated scene 2 to reflect that. Added collectibleitem prefab.

--- Assets/Scripts/VictoryConditions.cs
+++ Assets/Scripts/VictoryConditions.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum VictoryType
 {
@@ -19,20 +20,40 @@ public class VictoryConditions : MonoBehaviour {
     public GameObject[] LocationsToTouch;
     
     public int TriggerRange = 2;
+    
+    private HashSet<GameObject> ItemsCollected;
 
 	// Use this for initialization
 	void Start () {
-	
+        ItemsCollected = new HashSet<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(VictoryType == VictoryType.ArriveAtDestination)
+        if(VictoryType == VictoryType.ArriveAtDestination && Destination != null)
         {
             if(Vector3.Distance(transform.position, Destination.transform.position) <= TriggerRange)
             {
                 //Victory
-                Debug.Log("You win.");
+                Debug.Log("You win by arriving at the destination.");
+            }
+        }
+        
+        if(VictoryType == VictoryType.CollectAllItems && ItemsToCollect.Length > 0)
+        {
+            foreach(GameObject go in ItemsToCollect)
+            {
+                if(Vector3.Distance(transform.position, go.transform.position) <= TriggerRange)
+                {
+                    ItemsCollected.Add(go);
+                    go.SetActive(false);
+                }
+            }
+            
+            if(ItemsCollected.Count == ItemsToCollect.Length)
+            {
+                //Victory
+                Debug.Log("You win by having collected all the items!");
             }
         }
 	}
