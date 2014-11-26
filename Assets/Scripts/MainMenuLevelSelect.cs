485063d98f44bf3fc0576b671d4d8769e1926955
Mihail Morosan <mmoro001@cgs-macd-15506.campus.goldsmiths.ac.uk>
2014-11-26 10:24:23 +0000

    Added a main menu

---
+++ Assets/Scripts/MainMenuLevelSelect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuLevelSelect : MonoBehaviour {
+
+    public string LevelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    
+    void OnMouseDown() {
+        if(LevelToLoad.Length > 0)
+        {
+            Application.LoadLevel(LevelToLoad);
+        }
+    }
+}
