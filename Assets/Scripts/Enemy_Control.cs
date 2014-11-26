4506be7fc337d439360b647f06a242445b9276b2
andrei <iacob.andrei986@gmail.com>
2014-11-26 16:51:41 +0000

    adding health bars to enemies

--- Assets/Scripts/Enemy_Control.cs
+++ Assets/Scripts/Enemy_Control.cs
@@ -11,15 +11,20 @@ public class Enemy_Control : MonoBehaviour
     public TextMesh healthText;
     public Player_Control player;
     public AudioClip punchSound;
-
+    public int maxHealth = 10;
+    public float healthBarLength;
+    public int healthBarWidth; 
+    public int healthBarHeight; 
+    public GUISkin healthBarSkin; 
+    public Vector3 screenPosition;
 
     // Use this for initialization
     void Start()
     {
         power = 1;
-        health = 5;
+        health = 10;
         setHealthText();
-
+        healthBarLength = Screen.width / 16;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Control>();
 
     }
@@ -40,7 +45,10 @@ public class Enemy_Control : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
+        screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        screenPosition.y = Screen.height - screenPosition.y;
 
+        AddjustCurrentHealth(0);
             
 
         
@@ -56,6 +64,7 @@ public class Enemy_Control : MonoBehaviour
             
             audio.PlayOneShot(punchSound);
             player.health -= power;
+            AddjustCurrentHealth(-3);
             setHealthText();
             if (player.health < 1)
             {
@@ -69,4 +78,21 @@ public class Enemy_Control : MonoBehaviour
     {
         healthText.text = "Health: " + health.ToString();
     }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(screenPosition.x - 36, screenPosition.y - 35, Screen.width / 16, 7), "Health");
+        GUI.Box(new Rect(screenPosition.x - 36, screenPosition.y - 35, healthBarLength, 7), "Health");
+    }
+
+    public void AddjustCurrentHealth(int adj) {
+     health += adj;
+     if (health < 1)
+     {
+         healthBarLength = 0;
+         active = false;
+         Destroy(this);
+     }
+     healthBarLength = (Screen.width / 16 ) * (health / (float)maxHealth);
+ }
 }
